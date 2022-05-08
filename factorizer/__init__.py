67555206e0cd1985f56a1acfb42c0e0d0ca71e8f
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-05-08 13:49:24 +0200

    Add problem objective

---
+++ factorizer/__init__.py
@@ -0,0 +1,4 @@
+# Directions
+D = ["right", "left", "up", "down"]
+# Splitter fragments
+F_s = ["left", "right"]
--- factorizer/problem.py
+++ factorizer/problem.py
@@ -1,8 +1,24 @@
+from typing import Dict
+
 from pulp import *
+import networkx as nx
+
+from factorizer import D, F_s
 
 
-def build_problem(V, D, R_u, F_s, A, B) -> LpProblem:
-    """Build the problem from the instance."""
+def build_problem(G: nx.DiGraph, R_u: int, c: Dict) -> LpProblem:
+    """Build the problem from the instance.
+
+    Args:
+        G: The graph of the instance.
+        R_u: The maximum range of underground belts.
+    """
+    V = [v for v in G.nodes if G.nodes[v]["grid"]]
+    B = [v for v in G.nodes if not G.nodes[v]["grid"] and G.nodes[v]["start"]]
+    E = [v for v in G.nodes if not G.nodes[v]["grid"] and G.nodes[v]["end"]]
+
+    A = G.edges
+
     problem = LpProblem("Factorio Balancer", LpMinimize)
 
     # --- VARIABLES ---
@@ -11,7 +27,7 @@ def build_problem(V, D, R_u, F_s, A, B) -> LpProblem:
     t = {(v, d): LpVariable(f"t_{v}_{d}", cat=LpBinary) for v in V for d in D}
 
     # Build underground belt with range r at node v in direction d?
-    u = {(v, d, r): LpVariable(f"u_{v}_{d}_{r}", cat=LpBinary) for v in V for d in D for r in R_u}
+    u = {(v, d, r): LpVariable(f"u_{v}_{d}_{r}", cat=LpBinary) for v in V for d in D for r in range(1, R_u)}
 
     # Build splitter fragment f at node v?
     # For now, we assume splitters always point rightwards
@@ -22,3 +38,10 @@ def build_problem(V, D, R_u, F_s, A, B) -> LpProblem:
 
     # Activate arc a?
     y = {a: LpVariable(f"y_{a}", cat=LpBinary) for a in A}
+
+    # --- OBJECTIVE ---
+
+    problem += lpSum(((c["transport"] * t[v, d] + (c["underground"] * u[v, d, r] for r in range(1, R_u))) for d in
+                      D + (c["splitter"] * s[v, f] for f in F_s)) for v in V)
+
+    return problem
