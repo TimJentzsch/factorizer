76fc69b5cfc1de9853f71dc9a731476d8d4a091a
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-05-09 21:18:34 +0200

    Provide grid and belt sizes via CLI arguments

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import argparse
+import math
 import os.path
 from datetime import datetime
+from typing import List
 
 import networkx as nx
 
@@ -20,17 +23,40 @@ c = {
     "splitter": (7.5 + 16) / 2,
 }
 
-if __name__ == '__main__':
-    n = 5
-    m = 3
 
-    B = [1, 2]
-    E = [1, 2]
+def _centered_tiles(grid_height: int, belt_height: int) -> List[int]:
+    edge_room = math.floor((grid_height - belt_height) / 2.0)
+
+    return [i for i in range(edge_room, edge_room + belt_height)]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Factorio belt balancer")
+
+    parser.add_argument("-b", "--balancer", default="4x4",
+                        help="The size of the balanced belts. nxm means n input belts and m output belts.")
+    parser.add_argument("-g", "--grid", default="10x4",
+                        help="The size of the grid. nxm means a grid n tiles wide and m tiles high.")
+
+    args = parser.parse_args()
+
+    belt_size = args.balancer.split("x")
+    belt_n = int(belt_size[0])
+    belt_m = int(belt_size[1])
+
+    grid_size = args.grid.split("x")
+    grid_n = int(grid_size[0])
+    grid_m = int(grid_size[1])
+
+    edge_room_left = (grid_m - belt_m) / 2.0
+
+    B = _centered_tiles(grid_m, belt_n)
+    E = _centered_tiles(grid_m, belt_m)
 
     print("Building graph...")
     start = datetime.now()
 
-    G = build_graph(n, m, B, E, R)
+    G = build_graph(grid_n, grid_m, B, E, R)
 
     end_graph = datetime.now()
     dur_graph = (end_graph - start).total_seconds()
